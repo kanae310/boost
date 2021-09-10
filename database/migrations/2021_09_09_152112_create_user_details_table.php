212d30a434afddc84f99271c89726dd6734cf726
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-09-10 11:43:09 +0900

    UserDetailModelとマイグレーションを作成

---
+++ database/migrations/2021_09_09_152112_create_user_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->user_detail_id();
+            $table->char('birthday', 225);
+            $table->char('university_code', 225);
+            $table->char('profile_img', 225);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+}

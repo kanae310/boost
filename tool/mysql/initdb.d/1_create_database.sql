94d724bb5e9b19ca44b984fe0ca3010de79f738b
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-09-11 23:10:18 +0900

    user_idからidに変更して、認証エラーを解決

--- tool/mysql/initdb.d/1_create_database.sql
+++ tool/mysql/initdb.d/1_create_database.sql
@@ -1,4 +1,4 @@
 CREATE TABLE users(
-                      user_id INTEGER AUTO_INCREMENT,
+                      id INTEGER AUTO_INCREMENT,
                       user_name VARCHAR(20) NOT NULL UNIQUE,
-                      PRIMARY KEY (user_id));
+                      PRIMARY KEY (id));

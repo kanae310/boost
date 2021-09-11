73e54cfa98fdb1a68603c282ba2367e943456bf5
kanae310 <cgsd1088@mail4.doshisha.ac.jp>
2021-09-11 14:23:59 +0900

    [start]認証機能の初期構築完了

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', 'HelloController@index');
-Route::get('/user', 'HelloController@user');
-Route::get('/event/detail', 'EventController@detail');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';

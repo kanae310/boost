c0430b4333ae3049ac1ed9fa4d88ad143b2015f5
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-09-11 14:32:15 +0900

    シーダーでダミーデータを10件入れた

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,35 +17,66 @@ class UserSeeder extends Seeder
         DB::table('users')->truncate();
 
         DB::table('users')->insert([
-            'user_id' => 1,
-            'user_name' => '名前1',
-            'email' => 'hoge1@hoge',
-            'password' => 'hogehoge',
-        ],
-        [
-            'user_id' => 2,
-            'user_name' => '名前2',
-            'email' => 'hoge2@hoge',
-            'password' => 'hogehoge',
-        ],
-        [
-            'user_id' => 3,
-            'user_name' => '名前3',
-            'email' => 'hoge3@hoge',
-            'password' => 'hogehoge',
-        ],
-        [
-            'user_id' => 4,
-            'user_name' => '名前4',
-            'email' => 'hoge4@hoge',
-            'password' => 'hogehoge',
-        ],
-        [
-            'user_id' => 5,
-            'user_name' => '名前5',
-            'email' => 'hoge4@hoge',
-            'password' => 'hogehoge',
-        ]
-        );
+            [
+                'user_id' => 1,
+                'user_name' => '名前1',
+                'email' => 'hoge1@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 2,
+                'user_name' => '名前2',
+                'email' => 'hoge2@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 3,
+                'user_name' => '名前3',
+                'email' => 'hoge3@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 4,
+                'user_name' => '名前4',
+                'email' => 'hoge4@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 5,
+                'user_name' => '名前5',
+                'email' => 'hoge4@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 6,
+                'user_name' => '名前6',
+                'email' => 'hoge6@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 7,
+                'user_name' => '名前7',
+                'email' => 'hoge7@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 8,
+                'user_name' => '名前8',
+                'email' => 'hoge8@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 9,
+                'user_name' => '名前9',
+                'email' => 'hoge9@hoge',
+                'password' => 'hogehoge',
+            ],
+            [
+                'user_id' => 10,
+                'user_name' => '名前10',
+                'email' => 'hoge10@hoge',
+                'password' => 'hogehoge',
+            ],
+        ]);
     }
 }

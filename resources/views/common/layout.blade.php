8b670970bcb67b3a33e6a741c8aa5409721a2a98
kanae310 <cgsd1088@mail4.doshisha.ac.jp>
2021-09-12 22:48:04 +0900

    css初期設定完了

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -9,11 +9,17 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/mdb.min.css') }}"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    　　<!--cssのなかで一番下に置く-->
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
@@ -22,9 +28,11 @@
         <div class="container-fluid">
             @if (Route::has('login'))
                 @auth
-                {{-- ここからログイン後--}}
+                    {{-- ここからログイン後--}}
                     <a class="nav-contents" href="/event"
-                    style="font-size:30pt;font-weight:bold; text-decoration:none;">Lian</a>
+                       style="font-size:30pt;font-weight:bold; text-decoration:none;">
+                        <img src="{{ asset('img/testlogo.png') }}" alt="">
+                    </a>
                     <a class="nav-contents" href="/user">マイページ</a>
                     <a class="nav-contents" href="/event/category">カテゴリ一覧</a>
                     <a class="nav-contents" href="/event/create">イベント作成</a>
@@ -37,10 +45,11 @@
                             {{ __('Log Out') }}
                         </x-dropdown-link>
                     </form>
-                {{-- ここまで--}}
+                    {{-- ここまで--}}
                 @else
-                {{--ここからログイン前--}}
-                    <a class="nav-contents" href="/" style="font-size:30pt;font-weight:bold; text-decoration:none;">Lian</a>
+                    {{--ここからログイン前--}}
+                    <a class="nav-contents" href="/"
+                       style="font-size:30pt;font-weight:bold; text-decoration:none;">Lian</a>
                     <a class="nav-contents" href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log
                         in</a>
 
@@ -48,11 +57,12 @@
                         <a class="nav-contents" href="{{ route('register') }}"
                            class="ml-4 text-sm text-gray-700 underline">Register</a>
                     @endif
-                {{--ここまで--}}
+                    {{--ここまで--}}
                 @endauth
             @endif
         </div>
     </nav>
+
     <main class="py-4">
         @yield('index')
     </main>

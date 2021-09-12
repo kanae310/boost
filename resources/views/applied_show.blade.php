066e999d27f3a34d6e73179f2ef2ee720634c809
Ririka Umeno <ririponzu12@gmail.com>
2021-09-12 20:25:44 +0900

    申し込んだイベント一覧のview

---
+++ resources/views/applied_show.blade.php
@@ -0,0 +1,14 @@
+@extends('common.layout')
+
+@section('index')
+
+    <div>
+        @foreach ($applied_show as $event)
+            <div>
+                <a href="/event/detail/{{$event['event_id']}}">{{$event['event_name']}}</a>
+                <p>募集期間　{{$event['application_period']}}</p>
+                <p>開催場所　{{$event['location']}}</p>
+            </div>
+        @endforeach
+    </div>
+@endsection

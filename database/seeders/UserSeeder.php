d96f322d8f13307447806b6b351c5a354e320d41
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-09-12 11:05:56 +0900

    ユーザーのパスワードを暗号に

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,31 +21,31 @@ class UserSeeder extends Seeder
                 'id' => 1,
                 'user_name' => '名前1',
                 'email' => 'hoge1@hoge',
-                'password' => 'hogehoge',
+                'password' => '$2y$10$Kd.QNFr9fF6WZeo7.pCoF.sNlGHIDUBSHLn1Ga6GyvO/ePZAzSGny',
             ],
             [
                 'id' => 2,
                 'user_name' => '名前2',
                 'email' => 'hoge2@hoge',
-                'password' => 'hogehoge',
+                'password' => '$2y$10$4gL4fmA0RA/DhHdBYKP66OnYV8WzwnzOPMnwEe2dASNu/oX8889ni',
             ],
             [
                 'id' => 3,
                 'user_name' => '名前3',
                 'email' => 'hoge3@hoge',
-                'password' => 'hogehoge'
+                'password' => '$2y$10$zVabMyo/m4AtSEdFBGXlyut3AwP/qHJYZOjcsuptV7d9qC0bJb/IO'
             ],
             [
                 'id' => 4,
                 'user_name' => '名前4',
                 'email' => 'hoge4@hoge',
-                'password' => 'hogehoge',
+                'password' => '$2y$10$cquLxaBZNVbDi.ElxfODK.r2VbJTPjKxWvUfUDWkTyED5YPEKRIwG',
             ],
             [
                 'id' => 5,
                 'user_name' => '名前5',
-                'email' => 'hoge4@hoge',
-                'password' => 'hogehoge',
+                'email' => 'hoge5@hoge',
+                'password' => '$2y$10$wR4KtnizwWlH9wzT4SDrxeOuP/pAPJ7wKA7GClvOMevvMSrpHiksW',
             ],
             [
                 'id' => 6,

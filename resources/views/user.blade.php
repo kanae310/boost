0cae554ed8aacb868b62e97d4295699ca4eab989
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-09-13 15:03:46 +0900

    マイページ画面のデザイン作成

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,23 +1,27 @@
 @extends('common.layout')
 
 @section('index')
-    <div class="mx-auto" style="width: 200px;">
-        <img src="{{$user_data['profile_img']}}" class="rounded-circle">
-        <p>{{$user_data['user_name']}}</p>
-        @if ($user_data['gender'])
+    <div class="detail-content" style="width: 300px;">
+        <div class="center-block">
+            <img  src="{{$user_data['profile_img']}}" class="rounded-circle mx-auto">
+        </div>
+        <div class="pull-left">
+            <p style="font-size:15pt;font-weight:bold;margin-top:10px">{{$user_data['user_name']}}</p>
+            @if ($user_data['gender'])
             <p>性別　　　女</p>
-        @else
+            @else
             <p>性別　　　男</p>
-        @endif
-        <p>生年月日　{{$user_data['birthday']}}</p>
-        <p>大学名　　{{$user_data['university_name']}}</p>
-    </div>
-    <div class="mx-auto" style="width: 200px;">
-        <!-- <button class="primary">申し込んだイベント一覧</button>
-        <button class="primary">申し込んだイベント一覧</button>
-        <button class="primary">過去に参加したイベント一覧</button> -->
-        <a href="/user/event/applied">申し込んだイベント一覧</a><br>
-        <a href="/user/event/post-show">投稿したイベント一覧</a><br>
-        <a href="/user/event/past-applied">過去に参加したイベント一覧</a>
+            @endif
+            <p>生年月日　{{$user_data['birthday']}}</p>
+            <p>大学名　　{{$user_data['university_name']}}</p>
+        </div>
+        <div class="pull-left" style="width: 300px;">
+            <!-- <button class="primary">申し込んだイベント一覧</button>
+            <button class="primary">申し込んだイベント一覧</button>
+            <button class="primary">過去に参加したイベント一覧</button> -->
+            <a class="btn btn-secondary btn-rounded" href="/user/event/applied" style="margin-bottom:5px">申し込んだイベント一覧</a><br>
+            <a class="btn btn-secondary btn-rounded" href="/user/event/post-show" style="margin-bottom:5px">投稿したイベント一覧</a><br>
+            <a class="btn btn-secondary btn-rounded" href="/user/event/past-applied" style="margin-bottom:5px">過去に参加したイベント一覧</a>
+        </div>
     </div>
 @endsection
